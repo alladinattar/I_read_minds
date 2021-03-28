7f929016906aa3b40e197a9481714ef3e82f00a1
rinat <rinamuka4@gmail.com>
2021-03-28 01:49:13 -0700

    connected boost and create simple http server

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,23 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 Mukaev Rinat <rinamuka4@gmail.com>
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/config.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
 
 #endif // INCLUDE_HEADER_HPP_
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,3 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 Rinat Mukaev <rinamuka4@gmail.com>
 
-#include <header.hpp>
+#include <boost/beast.hpp>
